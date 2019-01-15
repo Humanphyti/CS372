8b511e0d103fca63126d62bd111bf2c8399f9c97
Peter Tucker <ptucker@whitworth.edu>
2019-01-15 13:29:56 -0800

    feedback

--- Homework2/src/Employee.java
+++ Homework2/src/Employee.java
@@ -1,6 +1,8 @@
 
 public interface Employee {
+	//PT -- ID shouldn't be a member of the interface. It should be implemented in Police, Teacher. -1
 	int ID = 0;
+	//PT -- these functions don't need to take a parameter. -2
 	int getID(Employee emp);
 	
 	boolean getMoney(Employee emp);
