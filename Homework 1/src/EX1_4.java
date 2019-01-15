543156f3a6bb7ecda4b8e21b424938594a70620c
Peter Tucker <ptucker@whitworth.edu>
2019-01-15 13:22:54 -0800

    feedback

--- Homework 1/src/EX1_4.java
+++ Homework 1/src/EX1_4.java
@@ -8,6 +8,8 @@ public class EX1_4 {
 		System.out.printf("Please enter your weight in pounds: ");
 		scnr = new Scanner(System.in);
 		double weight = scnr.nextDouble();
+		//PT -- validate the input. -2
+		//PT -- name these values: final double INCHES_PER_METER = 39.37;
 		double heightm = height / 39.37;
 		double weightm = weight / 2.2;
 		double BMI = weightm / java.lang.Math.pow(heightm, 2);
